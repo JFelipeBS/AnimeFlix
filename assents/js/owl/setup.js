67ddd88d0c9ffae425846015fcba3e32d83a8e80
Felipe Silva <f.filipisilva13@hotmail.com>
2022-01-30 20:53:54 -0300

    projeto_Criado

---
+++ assents/js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    items:1,
+    margin:10,
+
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:7
+        }
+    }
+})
